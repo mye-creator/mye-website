5f261b9417f5be680be9b356b69743ece4d7bf60
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-10-19 08:09:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-// some code.. 
+<style>
+body {
+  text-align: center;
+  margin: 0;
+}
+.header-1 {
+  background-image: url("img/background.jpg");
+  background-color: #cccccc;
+  height: 275px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  filter: sarelative(1.3), blur(2px);
+}
+.image-header {
+  text-align: center;
+  border-radius: 100%;
+  box-shadow: 10px;
+  position: sticky;
+  filter: saturate(1.3);
+}
+.text {
+  font-family: Apple Chansery, cursive;
+  color: #FFF7ED;
+  position: absolute;
+  text-align: center;
+  font-size: 45px;
+  right: 118px;
+  top: 155px;
+}
+</style>
+
 }
