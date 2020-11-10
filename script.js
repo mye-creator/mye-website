3ab14d663697f9ba8eba41a855cc8e5d3bec924b
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-10 16:45:28 +0600

    update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-$(".urut").change(function() {
-  $.ajax({
-    url: "<?php echo base_url(); ?>categories/brand/<?= $link_brand; ?>?l=<?= $l; ?>&h=<?= $h; ?>&city=<?= $city; ?>&city_name=<?= $city_name; ?>&ket=view",
-    type: "POST",
-    data: "urut=" + $(".urut").val(),
-    success: function(data) {
-      $("#result").html(data);
-    }
-  })
-})
+window.location.href="about.html";
